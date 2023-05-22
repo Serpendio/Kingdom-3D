0b57d5fc612c9e0de14b88de689d9e9a9ac4ce6c
Serpendio <58090288+Serpendio@users.noreply.github.com>
2023-05-22 22:44:41 +1200

    Renamed Camera class to stop clashing names

--- Kingdom3D/Assets/Scripts/GeneralLogic/PlayerCamera.cs
+++ Kingdom3D/Assets/Scripts/GeneralLogic/PlayerCamera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Camera : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] float sensitivityY;
     [SerializeField] float sensitivityX;
