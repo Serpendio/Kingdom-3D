197f4bcb97941dad7a7baafe7750491864a8410d
Serpendio <58090288+Serpendio@users.noreply.github.com>
2023-05-22 23:20:43 +1200

    Began a portal shader
    Put on hold once I realised it wasn't priority.

--- Kingdom3D/Assets/Scripts/Game/Portal/PortalGraphics.cs
+++ Kingdom3D/Assets/Scripts/Game/Portal/PortalGraphics.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class PortalGraphics : MonoBehaviour
 {
     // alembic doesn't work for webGL, add backup for that case
+    // to increase performance, only one texture could to be used, however this won't work when both are visible on screen
+
+    [SerializeField] Camera mainCam, linkedCam1, linkedCam2;
+    [SerializeField] Transform surface1, surface2;
+    [SerializeField] RenderTexture portalTexFile;
+    private RenderTexture portalTexture1, portalTexture2;
 
     void Start()
     {
-        
+        if (mainCam == null)
+        {
+            Debug.LogWarning("No main camera has been assigned, this could affect performance");
+            mainCam = Camera.main;
+        }
+        portalTexture1 = Instantiate(portalTexFile);
+        linkedCam1.targetTexture = portalTexture1;
+        MaterialPropertyBlock props = new MaterialPropertyBlock();
+        props.SetTexture("_MainTex", portalTexture1);
+        surface1.GetComponent<MeshRenderer>().SetPropertyBlock(props);
+
+        portalTexture2 = Instantiate(portalTexFile);
+        linkedCam2.targetTexture = portalTexture2;
+        props = new MaterialPropertyBlock();
+        props.SetTexture("_MainTex", portalTexture2);
+        surface2.GetComponent<MeshRenderer>().SetPropertyBlock(props);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        linkedCam1.transform.rotation = linkedCam2.transform.rotation = mainCam.transform.rotation;
+
+        linkedCam1.transform.position = mainCam.transform.position - surface2.transform.position + surface1.transform.position;
+        linkedCam2.transform.position = mainCam.transform.position - surface1.transform.position + surface2.transform.position;
     }
 }
